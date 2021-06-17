c8204154fb8aada26210cda589763750faa2a54d
yuri-misonoo <yuri.panpanda@gmail.com>
2021-06-17 06:00:43 +0000

    [Start]2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -16,6 +16,10 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
 
   private
   def list_params
